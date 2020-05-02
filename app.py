394c83be8ea98caa60af7fdca2c8ec34af67c33f
neeraj <neerajgupta2407@gmail.com>
2020-05-02 14:50:59 +0530

    1.) Added correct modules/lib to be install as installing requirement.txt is throwing error. 2) Changed the path of callback url to root from /sms

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
-import prediction
 from twilio.twiml.messaging_response import MessagingResponse
+from Prediction import detecting_fake_news
 
 app = Flask(__name__)
-@app.route('/sms', methods=['POST'])
+@app.route('/', methods=['POST'])
 def sms():
+
     resp = MessagingResponse()
     inbMsg = request.values.get('Body')
-    pred, confidence = prediction.detecting_fake_news(inbMsg)
+    # print(inbMsg)
+    pred, confidence = detecting_fake_news(inbMsg)
 
     resp.message(
         f'The news headline you entered is {pred[0]!r} and corresponds to {confidence[0][1]!r}.')
